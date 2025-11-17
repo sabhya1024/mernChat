b99c467a86e9312af8f15f762c2a9a4f360ac459
sabhya1024 <dhimansabhya@gmail.com>
2025-11-17 14:38:35 +0530

    project completed

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
 import cors from 'cors'
-
+import path from 'path'
 
 import connectDB from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
@@ -13,7 +13,7 @@ import {app, server} from './lib/socket.js'
 dotenv.config()
 
 const PORT = process.env.PORT
-
+const __dirname = path.resolve()
 
 app.use(helmet());
 
@@ -28,6 +28,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"))
+  })
+}
 
 const startSever = async () => {
   try {
