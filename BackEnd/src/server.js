711264d423242c26511dfd46378ed83fb7957661
sabhya1024 <dhimansabhya@gmail.com>
2025-11-19 22:47:46 +0530

    regex error resolved

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -12,7 +12,7 @@ import messageRoutes from "./routes/message.route.js"
 import {app, server} from './lib/socket.js'
 dotenv.config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
