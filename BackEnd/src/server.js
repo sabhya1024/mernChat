169ffd53bfb91d554d1898b34d2e9e874e52a6e5
sabhya1024 <dhimansabhya@gmail.com>
2025-11-19 22:34:31 +0530

    regex error resolved

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -33,7 +33,7 @@ app.use("/api/messages", messageRoutes)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "../FrontEnd/dist")))
 
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../FrontEnd", "dist", "index.html"))
   })
 }
